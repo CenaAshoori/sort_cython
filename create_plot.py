c34856f4f76eecc2c4e82ab33295f638391e31eb
Cena Ashoori <52758031+CenaAshoori@users.noreply.github.com>
2021-12-23 20:13:31 +0330

    update

--- create_plot.py
+++ create_plot.py
@@ -1,65 +1,76 @@
 import matplotlib.pyplot as plt
-import matplotlib.colors 
+import matplotlib.colors
 from PIL import ImageColor
 import matplotlib.pyplot as plt
 import numpy as np
 
-def save_plot(n,text,times ):
+
+def save_plot(n, text, times):
     palett = """
-#172774
-#FF0075
-#EEEEEE
 #77D970
+#FF0075
+#172774
+#EEEEEE
 """
-    txt_1,txt_2,txt_3 = text
-    t1,t2,t3 = times
-    palett = list(map(lambda x:"#"+x.strip(),palett.split("#")[1:]))
+    txt_1, txt_2, txt_3 = text
+    t1, t2, t3 = times
+    palett = list(map(lambda x: "#"+x.strip(), palett.split("#")[1:]))
     print(palett)
-    # n = [2, 10, 100, 500, 1000]
-    # t1 = [0, 1 , 2, 3 ,4]
-    # t2 = [2, 4 , 5, 11 ,23]
-    n = list(map(lambda x:str(x),n))
 
-
-    width = .35      # the width of the bars: can also be len(x) sequence
-
+    n_labal = list(map(lambda x: str(x), n))
+    n = np.array(n)
+    min = n[1]-n[0] if len(n) > 1 else 1
+    for i in range(0,len(n)-1):
+        if n[i+1]-n[i] < min:
+            min= n[i+1]-n[i]
+    
+    min -= 40
 
     for i in range(len(palett)):
         fig, ax = plt.subplots()
-        color_a = palett[(i+0)%len(palett)]
-        color_b = palett[(i+1)%len(palett)]
-        color_c = palett[(i+2)%len(palett)]
-        color_d = palett[(i+3)%len(palett)]
+        color_a = palett[(i+0) % len(palett)]
+        color_b = palett[(i+1) % len(palett)]
+        color_c = palett[(i+2) % len(palett)]
+        color_d = palett[(i+3) % len(palett)]
 
-        r,g,b = ImageColor.getcolor(color_a, "RGB")
-        print (r,g,b)
+        r, g, b = ImageColor.getcolor(color_a, "RGB")
+        print(r, g, b)
         yiq = ((r*299)+(g*587)+(b*114))/1000
         text_color = "black" if yiq >= 128 else "white"
         print(text_color)
-        plt.rcParams['text.color'] ='#{:02x}{:02x}{:02x}'.format(((255-r)%255), ((255-g)%255),((255-b)%255),)
-        plt.rcParams['xtick.color'] ='#{:02x}{:02x}{:02x}'.format(((255-r)%255), ((255-g)%255),((255-b)%255),)
-        plt.rcParams['ytick.color'] ='#{:02x}{:02x}{:02x}'.format(((255-r)%255), ((255-g)%255),((255-b)%255),)
+        plt.rcParams['text.color'] = '#{:02x}{:02x}{:02x}'.format(
+            ((255-r) % 255), ((255-g) % 255), ((255-b) % 255),)
+        plt.rcParams['xtick.color'] = '#{:02x}{:02x}{:02x}'.format(
+            ((255-r) % 255), ((255-g) % 255), ((255-b) % 255),)
+        plt.rcParams['ytick.color'] = '#{:02x}{:02x}{:02x}'.format(
+            ((255-r) % 255), ((255-g) % 255), ((255-b) % 255),)
         # plt.rcParams['board.color'] ='#{:02x}{:02x}{:02x}'.format(((255-r)%255), ((255-g)%255),((255-b)%255),)
-        mokhalef = '#{:02x}{:02x}{:02x}'.format(((255-r)%255), ((255-g)%255),((255-b)%255),)
+        mokhalef = '#{:02x}{:02x}{:02x}'.format(
+            ((255-r) % 255), ((255-g) % 255), ((255-b) % 255),)
         ax.spines["top"].set_color(mokhalef)
         ax.spines["right"].set_color(mokhalef)
         ax.spines["left"].set_color(mokhalef)
         ax.spines["bottom"].set_color(mokhalef)
         ax.spines["bottom"].set_linestyle("-")
-        
-        plt.rcParams['axes.labelcolor'] =mokhalef
-        ax.bar(n, t1, width,  label=txt_1,color=color_a)
-        ax.bar(n, t2, width,  bottom=t1,
-            label=txt_2,color=color_b)
-        ax.bar(n, t3, width,  bottom=t2,
-            label=txt_3,color=color_c)
 
-        ax.set_ylabel('Execution Time')
-        ax.set_xlabel('Input')
+        plt.rcParams['axes.labelcolor'] = mokhalef
+
+        c      = ax.bar(n - min/3, t1, min/3,  label=txt_1, color=color_a)
+        cython = ax.bar(n        , t2, min/3,   label=txt_2, color=color_b)
+        python = ax.bar(n + min/3, t3, min/3,  
+                        label=txt_3, color=color_c)
+
+        ax.set_ylabel('Execution time(ms)')
+        ax.set_xlabel('Input size')
         ax.set_title('Runtime by input size')
+        ax.set_xticks(n, n_labal)
+
+        ax.bar_label(c, padding=3)
+        ax.bar_label(cython, padding=3)
+        ax.bar_label(python, padding=3)
         ax.legend()
 
         plt.savefig("final/output"+f"_{i}", facecolor=color_d, bbox_inches="tight",
                     pad_inches=0.3, transparent=True)
-        # break
+        break
     plt.show()
--- t.py
+++ t.py
@@ -1,5 +1,5 @@
 
-from sort_c      import bubblesort_caller
+from sort_c import bubblesort_caller
 from sort_cython import bubblesort as cy_sort
 from sort_python import bubblesort as py_sort
 import matplotlib.pyplot as plt
@@ -9,34 +9,32 @@ import array
 import create_plot
 import numpy as np
 
-def compare_code(n:list):
-    t1 =[]
-    t2=[]
-    t3=[]
+
+def compare_code(n: list):
+    t1, t2, t3 = [], [], []
     ms_in_sec = 1000
 
     for i in n:
-        unsort = [random.randint(0,i) for _ in range(i)]
-        
+        unsort = [random.randint(0, i) for _ in range(i)]
+
         arr1 = array.array('i', unsort)
-        dur = timeit.timeit(lambda: bubblesort_caller.c_sort(arr1,i), number=1)
+        dur = timeit.timeit(
+            lambda: bubblesort_caller.c_sort(arr1, i), number=1)
         print(f"C[{i}]:", round(dur * ms_in_sec, 1), "ms")
-        t1.append(dur)
-        
+        t1.append(round(dur * ms_in_sec, 1))
+
         dur = timeit.timeit(lambda: cy_sort.bubbleSort(arr1), number=1)
         print(f"Cython[{i}]:", round(dur * ms_in_sec, 1), "ms")
-        t2.append(dur)
-        
+        t2.append(round(dur * ms_in_sec, 1))
+
         dur = timeit.timeit(lambda: py_sort.bubbleSort(unsort), number=1)
         print(f"Python[{i}]:", round(dur * ms_in_sec, 1), "ms")
-        t3.append(dur)
+        t3.append(round(dur * ms_in_sec, 1))
 
-    create_plot.save_plot(n,("C","Cython","Python"),(t1,t2,t3))
-
-
+    create_plot.save_plot(n, ("C", "Cython", "Python"), (t1, t2, t3))
 
 
-n = [100, 200,300, 400,]
-compare_code(n)
-# n = [ 500, 1000, 2000,4000]
+# n = [100, 300, 400, ]
 # compare_code(n)
+n = [ 500, 1000, 2000,4000]
+compare_code(n)
